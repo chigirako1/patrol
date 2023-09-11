a10fd88e65f55529a4efa2eb6bbfe549f406942c
cgr <chigira.koichi@gmail.com>
2023-09-11 11:06:53 +0900

    変更 	modified:   app/assets/stylesheets/common.css 	modified:   app/controllers/artists_controller.rb 	modified:   app/views/artists/_artist.html.erb 	modified:   app/views/artists/_form.html.erb 	modified:   app/views/artists/edit.html.erb 	modified:   app/views/artists/index.html.erb 	modified:   app/views/artists/show.html.erb 	new file:   app/views/layouts/application.html.erb 	new file:   config/routes.rb

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,41 +1,116 @@
+require 'nkf'
+require 'find'
+
 class ArtistsController < ApplicationController
   before_action :set_artist, only: %i[ show edit update destroy ]
 
   # GET /artists or /artists.json
   def index
     @size_per_table = 25
-    @num = params[:num]
+    twt = params[:twt]
+    ai = params[:ai]
+    amount = params[:amount]
+    year = params[:year]
+    display_number = params[:display_number]
+    group_by = params[:group_by]
+    sort_by = params[:sort_by]
 
-    #@artists_group = Artist.all.group_by {|elem| elem[:last_ul_datetime].year}.sort.reverse.to_h
-    #@artists_group = Artist.all.group_by {|elem| elem[:last_ul_datetime].strftime("%Y-%m")}.sort.reverse.to_h
-    artists_group = Artist.all.group_by {|elem| elem[:last_ul_datetime].strftime("%Y")}.sort.reverse.to_h
+    if display_number != nil
+      @size_per_table = display_number.to_i
+    end
+
     @artists_group = {}
-    artists_group.each do |key, artists|
-      if @num == "few"
-        artists = artists.sort_by {|x| [x[:last_access_datetime], x[:filenum]]}
+    artists = Artist.all
+
+    if twt == "twt"
+      artists = artists.select {|elem| elem[:twtid] != ""}
+    end
+
+    if ai == "ai"
+      artists = artists.select {|elem| elem[:comment] == "AI"}
+    end
+
+    if amount != nil
+      case amount 
+      when "many"
+        artists = artists.select {|elem| elem[:filenum] > 200}
+      when "mid"
+        artists = artists.select {|elem| elem[:filenum] > 100 and elem[:filenum] <= 200}
+      when "few"
+        artists = artists.select {|elem| elem[:filenum] <= 100}
       else
-        artists = artists.sort_by {|x| [x[:last_access_datetime], -x[:priority], -x[:filenum]]}
-      end
-      if artists.count > 1000
-        tmp = artists.group_by {|elem| elem[:last_ul_datetime].strftime("%Y-%m")}.sort.reverse.to_h
-        tmp.each do |tmp_key, tmp_artists|
-          @artists_group[tmp_key] = tmp_artists
-        end
-      else
-        @artists_group[key] = artists
       end
     end
 
+    if year != nil
+      artists = artists.select {|elem| elem[:last_ul_datetime].strftime("%Y") == year}
+    end
 
-    #@artists = Artist.all.sort {|a,b| a[:last_access_datetime] <=> b[:last_access_datetime]}
-    #@artists = Artist.all.sort_by(&:last_access_datetime, &:filenum)
-    @artists = Artist.all.sort_by {|x| [x[:last_access_datetime], -(x[:filenum] / 100 * 100)]}
-    #@artists = Artist.all.sort_by {|x| [x[:filenum]]}
+    case sort_by
+    when "access_date_X_last_ul_datetime"
+      artists = artists.sort_by {|x| [x[:last_access_datetime], x[:last_ul_datetime]]}
+    when "access_date_X_recent_filenum"
+      artists = artists.sort_by {|x| [x[:last_access_datetime], -x[:recent_filenum]]}
+    when "access_date_X_pxvname_X_recent_filenum"
+      artists = artists.sort_by {|x| [x[:last_access_datetime], x[:pxvname].downcase, -x[:recent_filenum]]}
+    else
+    end
+
+    case group_by
+    when "last_ul_datetime"
+      @artists_group = artists.group_by {|elem| elem[:last_ul_datetime].strftime("%Y")}.sort.reverse.to_h
+      return
+    when "pxvname"
+      @artists_group = artists.group_by {|elem| select_group(elem[:pxvname])}.sort.to_h
+      return
+    else
+    end
+
+    set_artist_group_pxv(artists)
   end
 
   # GET /artists/1 or /artists/1.json
   def show
     @artist.update(last_access_datetime: Time.now)
+
+    @path_list = []
+    rpath = ""
+    search_str = %!(#{@artist[:pxvid]})!
+    base_path = Rails.root.join("public").to_s
+
+=begin
+遅すぎる
+    Find.find(base_path + "/dpxv/pxv") {|f|
+      Find.prune if f.split(/\//).size > 11
+      @path_list << f
+      if f.include?(search_str)
+        rpath = f
+        break
+      end
+    }
+
+    findやglobはシンボリックリンクは辿ってくれない模様
+=end
+    #DBにパスを格納したいが面倒なので。。。　
+    txtpath = Rails.root.join("public/pxv/dirlist.txt").to_s
+    File.open(txtpath) { |file|
+      while line  = file.gets
+        if line.include?(search_str)
+          rpath = line.chomp
+          break
+        end
+      end
+    }
+
+    if rpath != ""
+      tmp_list = []
+      Find.find(rpath) do |path|
+        if File.extname(path) == ".jpg"
+          tmp_list << path.gsub(base_path, "")
+        end
+      end
+      @path_list = tmp_list.last(5).reverse
+    end
 
     #外部にリダイレクトしようとするとエラー
     #url = %!https://www.pixiv.net/users/#{@artist["pxvid"]}!
@@ -98,6 +173,65 @@ class ArtistsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def artist_params
-      params.require(:artist).permit(:pxvname, :pxvid, :filenum, :last_dl_datetime, :last_ul_datetime, :last_access_datetime, :priority)
+      params.require(:artist).permit(:pxvname, :pxvid, :filenum, :last_dl_datetime, :last_ul_datetime, :last_access_datetime, :priority, :status, :comment, :twtid)
+    end
+
+    def select_group(pxvname)
+      group = ""
+      fl = pxvname[0]
+      case fl
+      when /[A-Za-z]/
+        group = fl.downcase
+      when /\p{katakana}/
+        fl = NKF.nkf('-w -X', fl) #半角を全角に変換
+        fl = fl.tr('ァ-ン','ぁ-ん')
+        group = "あ"
+      when /\p{hiragana}/
+        group = "あ"
+      when /[:digit:]/
+        group = "0"
+      when /[0-9]/
+        group = "0"
+      when /[가-힣]/
+        group = "ハングル"
+      else
+        group = "他"
+      end
+      group
+    end
+
+    def judge_number(filenum)
+      if filenum >= 100
+        "3多"
+      elsif filenum >= 30
+        "2中"
+      elsif filenum >= 10
+        "1小"
+      else
+        "0"
+      end
+    end
+
+    def set_artist_group_pxv(artists_select)
+      artists_group_by_year = artists_select.group_by {|elem| elem[:last_ul_datetime].strftime("%Y")}.sort.reverse.to_h
+
+      artists_group_by_year.each do |key, artists|
+        if artists.count > 300
+          artists_group_by_yymm = artists.group_by {|elem| elem[:last_ul_datetime].strftime("%Y-%m")}.sort.reverse.to_h
+          artists_group_by_yymm.each do |tmp_key, tmp_artists|
+            if tmp_artists.count > 100
+              tmp2 = tmp_artists.group_by {|elem| judge_number(elem[:filenum])}.sort.reverse.to_h
+              tmp2.each do |tmp2_key, tmp2_artists|
+                @artists_group[tmp_key + ":" + tmp2_key.to_s] = tmp2_artists.sort_by {|x| [x[:last_access_datetime], -x[:recent_filenum]]}
+              end
+            else
+              @artists_group[tmp_key] = tmp_artists.sort_by {|x| [x[:last_access_datetime], -x[:filenum]]}
+            end
+          end
+        else
+          @artists_group[key] = artists.sort_by {|x| [x[:last_access_datetime], -x[:filenum]]}
+        end
+      end
+      @artists_group["all"] = Artist.all.sort_by {|x| [x[:last_access_datetime], -(x[:recent_filenum] / 10 * 10), x[:last_ul_datetime] ,-x[:filenum]]}
     end
 end
---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+  resources :artists
+  resources :users
+  root 'home#index'
+  get 'home/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/help', to: 'home#help'
+  get '/test', to: 'home#test'
+=begin
+  get '/books', to: 'books#index'
+  get '/books/new', to: 'books#new', as: :new_book
+  post '/books', to: 'books#create'
+  get '/books/:id', to: 'books#show', as: :book
+  get 'books/:id/edit', to: 'books#edit', as: :edit_book
+  patch '/books/:id', to: 'books#update'
+  delete '/books/:id', to: 'books#destroy'
+=end
+  resources :books do
+    resources :comments
+  end
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
